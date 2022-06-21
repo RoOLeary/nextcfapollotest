995d4f08a558797434be6535345e06ca00f039af
ronan <ronan@thenextweb.com>
2022-06-21 13:27:13 +0200

    attempt static build

--- apollo-client.js
+++ apollo-client.js
@@ -3,9 +3,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
     uri: "https://cities.local.tnw.dev/api",
     cache: new InMemoryCache(),
-    headers: {
-        Authorization: `Bearer 8YjnkaiY0aJ6rJQjwbT2wawiJPAC2PGE`,
-    },
+    // headers: {
+    //     Authorization: `Bearer 8YjnkaiY0aJ6rJQjwbT2wawiJPAC2PGE`,
+    // },
 });
 
 export default client;
