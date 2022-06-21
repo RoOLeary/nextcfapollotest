33a562942d4f780a9375459f6127c8faf57d3026
ronan <ronan@thenextweb.com>
2022-06-21 13:25:52 +0200

    modify client

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://cities.thenextweb.com/api",
+    uri: "https://cities.local.tnw.dev/api",
     cache: new InMemoryCache(),
     headers: {
         Authorization: `Bearer 8YjnkaiY0aJ6rJQjwbT2wawiJPAC2PGE`,
